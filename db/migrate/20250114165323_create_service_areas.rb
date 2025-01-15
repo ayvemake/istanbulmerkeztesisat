c0303b5d699575f3f7791650e44619c5d3f96385
ayvemake <anilcan.kahraman@gmail.com>
2025-01-15 14:14:35 +0100

    fixed problems about postal code and description

--- db/migrate/20250114165323_create_service_areas.rb
+++ db/migrate/20250114165323_create_service_areas.rb
@@ -2,7 +2,8 @@ class CreateServiceAreas < ActiveRecord::Migration[7.1]
   def change
     create_table :service_areas do |t|
       t.string :name
-      t.string :postal_code
+      t.text :description
+      t.string :postal_codes, array: true
 
       t.timestamps
     end
