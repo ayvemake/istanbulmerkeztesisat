7d37d7d7f90867a9883df72f43ed88cbc4a97926
ayvemake <anilcan.kahraman@gmail.com>
2025-02-03 22:44:55 +0100

    Configure for GitHub Pages deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,1 +1,8 @@
- 
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'istanbulmerkeztesisat.com', 'www.istanbulmerkeztesisat.com'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end 
