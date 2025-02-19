55d55d38db6d77e71176ab61e02374f0ca19a434
ayvemake <anilcan.kahraman@gmail.com>
2025-02-19 12:24:55 +0100

    Fix Node.js and Yarn configuration for Heroku

--- config/importmap.rb
+++ config/importmap.rb
@@ -13,12 +13,8 @@ pin 'controllers/dropdown_controller', to: 'controllers/dropdown_controller.js'
 pin 'controllers/lazy_image_controller', to: 'controllers/lazy_image_controller.js'
 pin 'controllers/mobile_menu_controller', to: 'controllers/mobile_menu_controller.js'
 pin 'controllers/navbar_controller', to: 'controllers/navbar_controller.js'
-pin 'controllers/photoswipe_controller', to: 'controllers/photoswipe_controller.js'
-pin 'controllers/water_effect_controller', to: 'controllers/water_effect_controller.js'
 pin 'controllers/whatsapp_controller', to: 'controllers/whatsapp_controller.js'
 
 # External dependencies
-pin 'photoswipe', to: 'https://ga.jspm.io/npm:photoswipe@5.3.7/dist/photoswipe.esm.js'
-pin 'photoswipe/lightbox', to: 'https://ga.jspm.io/npm:photoswipe@5.3.7/dist/photoswipe-lightbox.esm.js'
 pin '@rails/activestorage', to: 'https://ga.jspm.io/npm:@rails/activestorage@7.1.0/app/assets/javascripts/activestorage.esm.js'
 pin '@rails/actioncable', to: 'https://ga.jspm.io/npm:@rails/actioncable@7.1.0/app/assets/javascripts/actioncable.esm.js'
