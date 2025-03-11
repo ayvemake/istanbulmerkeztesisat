2638a71c43d3586163e26941c21f33375acca95f
ayvemake <anilcan.kahraman@gmail.com>
2025-03-11 15:19:05 +0100

    Fix Tailwind CSS configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,12 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './app/views/**/*.{erb,haml,html,slim}',
+    './public/*.html',
     './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/components/**/*.{erb,rb}',
     './config/initializers/simple_form.rb',
-    './app/components/**/*.{erb,haml,html,slim,rb}'
   ],
   theme: {
     extend: {
