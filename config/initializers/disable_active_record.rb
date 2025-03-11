641f641161ad56022ee0b0e19091c02910e5d240
ayvemake <anilcan.kahraman@gmail.com>
2025-03-11 15:53:34 +0100

    Fix ActiveRecord configuration

--- config/initializers/disable_active_record.rb
+++ config/initializers/disable_active_record.rb
@@ -2,4 +2,13 @@ Rails.application.config.middleware.delete(ActiveRecord::Migration::CheckPending
 
 # Désactiver les fonctionnalités liées à ActiveRecord
 Rails.application.config.app_middleware.delete(ActiveRecord::QueryCache) if defined?(ActiveRecord)
-Rails.application.config.app_middleware.delete(ActiveRecord::ConnectionAdapters::ConnectionManagement) if defined?(ActiveRecord) 
+
+# Configuration pour ActiveRecord
+if defined?(ActiveRecord)
+  ActiveSupport.on_load(:active_record) do
+    self.verbose_query_logs = Rails.env.development?
+    self.database_selector = { delay: 2.seconds }
+    self.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+    self.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  end
+end 
